5a2c7c38af81ebd99fdb6628be6cf0358f276982
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-20 19:00:19 +0530

    Solve Using Switch Statement

--- Day8EmpWageComputation/Program.cs
+++ Day8EmpWageComputation/Program.cs
@@ -11,6 +11,7 @@ namespace Day8EmpWageComputation
             empWageComputation.CheckEmpIsPresentOrNot();
             empWageComputation.CalcEmpDailyWage();
             empWageComputation.CheckEmpFullOrPartTime();
+            empWageComputation.UsingSwitchStatement();
         }
     }
 }
